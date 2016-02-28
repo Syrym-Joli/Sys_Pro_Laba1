4491c2a2d47ca84e47f8593703f5f533555a55b1
Syrym <www.sirim@mail.ru>
2016-02-29 04:40:25 +0600

    FnWorldLength(s,n)

---
+++ SP-laba1/Program.cs
@@ -0,0 +1,38 @@
+﻿//Fn WorldLength(s,n);
+//Основные слова: дружба, любовь , счастье?!
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP_laba1
+{
+    class Program
+    {
+        private int FnWorldLength(string s, int n)
+        {   
+            s = Console.ReadLine();
+            string[] words = s.Split(new[] { ' ', ',', ':', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in words)
+            {
+                
+                n = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("Кол-во букв: {0}", words[n].Length);
+
+            }
+            return (0);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**Функция WorldLength(s,n)**");
+            Console.Write("Введите предложение: ");
+            
+            Console.Write("Введите номер-слово: ");
+            
+
+            Console.ReadKey();
+        }
+        
+    }
+}
